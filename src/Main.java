a14471d3afc010dbbc817b7ed9f831a23f7d7483
Miguel Damaceno <miguel.araujo669@gmail.com>
2020-05-13 19:44:14 -0300

    Leitura da escolaridade

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
+		int escolaridade, experiencia;
+		char viajar, motorista;
+		
+		System.out.println("Qual sua escolaridade?");
+		System.out.println("1) Ensino Fundamental");
+		System.out.println("2) Ensino Médio");
+		System.out.println("3) Ensino Superior");
+		System.out.println("Digite uma opção: ");
+		escolaridade = sc.nextInt();
+		
+		sc.close();
 	}
 
 }
